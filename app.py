bc549adbf3b16ee43229a9d56a27b9ee50e9054e
str4ng3love <normalemailadress@gmail.com>
2023-06-02 16:07:29 +0200

    fetch test

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
-
+from flask import render_template
+import requests
+import json
+from types import SimpleNamespace
 app = Flask(__name__)
 
 @app.route("/")
-def hello_hello():
-    return "<p>Hello, World!</p>"
+def index():
+    tritSell = requests.get('https://esi.evetech.net/latest/markets/10000002/orders/?datasource=tranquility&order_type=sell&page=1&type_id=34')
+    cenaTrit = json.loads(tritSell.text)
+    print(cenaTrit)
+    return render_template('index.html')
