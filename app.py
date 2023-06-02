3a32e205fc4b6f0121bdcdc2d7a5eff6d6a774cc
str4ng3love <normalemailadress@gmail.com>
2023-06-02 17:33:58 +0200

    setting up templates and styles

--- app.py
+++ app.py
@@ -2,12 +2,22 @@ from flask import Flask
 from flask import render_template
 import requests
 import json
-from types import SimpleNamespace
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     tritSell = requests.get('https://esi.evetech.net/latest/markets/10000002/orders/?datasource=tranquility&order_type=sell&page=1&type_id=34')
-    cenaTrit = json.loads(tritSell.text)
-    print(cenaTrit)
+    headers = tritSell.headers
+    
+    # cenaTrit = json.loads(tritSell.text)
+    # print(cenaTrit[2]["price"])
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+
+    return render_template('about.html')
+@app.route('/salvager')
+def salvager():
+
+    return render_template('salvage.html')
