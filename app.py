07f3d9b43ca15b02fe6af20ec9c047bba91a07ab
str4ng3love <normalemailadress@gmail.com>
2023-06-03 08:38:49 +0200

    configured and working db

--- app.py
+++ app.py
@@ -6,23 +6,37 @@ from flask import render_template
 from prisma import Prisma, register
 from prisma.models import Item
 app = Flask(__name__)
+prisma = Prisma()
+from prisma.models import Item
 
+db = Prisma(auto_register=True)
+async def main() -> None:
+    await db.connect()
 
+    item = await Item.prisma().create(
+        data={
+            'name':'zenon',
+            'typeId':12038
+        }
+    )
+    await db.disconnect()
 
+    
 @app.route("/")
 
 def index():
-
-    return render_template('index.html')
+    # asyncio.run(main())
+    title = 'Eve Hobo'
+    return render_template('layout.html', title=title)
 
 @app.route('/about')
 def about():
 
-    return render_template('about.html')
+    return render_template('/about.html', title='Eve HObo | About')
 @app.route('/salvager')
 def salvager():
 
-    return render_template('salvage.html')
+    return render_template('salvage.html', title='Eve HObo | Salvager')
 
 @app.route('/hobo', methods=['GET', 'POST'])
 def create():
