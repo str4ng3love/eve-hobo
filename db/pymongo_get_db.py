597ce4f5758a60b0791a98d59006889914be7c8b
str4ng3love <101009597+str4ng3love@users.noreply.github.com>
2023-06-13 11:39:31 +0200

    Update pymongo_get_db.py

--- db/pymongo_get_db.py
+++ db/pymongo_get_db.py
@@ -1,11 +1,10 @@
 from pymongo import MongoClient
 from dotenv import dotenv_values
-
+import os
 config = dotenv_values(".env")
 def get_database():
-    CONNECTION_STRING = config['DATABASE_URL']
+    CONNECTION_STRING =   os.getenv("DATABASE_URL")
     client = MongoClient(CONNECTION_STRING)
-
     return client["salvager"]
 
 if __name__ == "__main__":
