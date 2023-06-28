aa04af5337fddb9cda8f9c189ed7745c657c3a45
str4ng3love <normalemailadress@gmail.com>
2023-06-28 09:44:26 +0200

    added routes serving sorted and filtered orders

--- controllers/handleRequests.py
+++ controllers/handleRequests.py
@@ -1,24 +1,38 @@
-from controllers.get_records import getOrdersBlob
-from utils.helpers import filter_jita44, concat, concatSameTypeIDs, sortByPriceAsc, sortByPriceDesc, sortByTypeID 
+from controllers.get_records import getOrdersBlob, getMaterials, getItems
+from utils.helpers import filter_jita44, concat, concatSameTypeIDs, sortByPriceAsc, sortByPriceDesc, sortByTypeID
 
 
-def find_profits(order_type, searchMethod, amount):
-    
+def get_filtered_and_sorted_orders(order_type):
+
     orders = getOrdersBlob(order_type)
     print("Concating...")
-    # concatedOrders = concat(orders)
-    # print("Filtering...")
-    # jitaOrders = list(filter(filter_jita44, concatedOrders))
-    # print("Sorting...")
-    # sortedOrders = sortByTypeID(jitaOrders)
-    # print("Concating same type_id orders...")
-    # sameOrders = concatSameTypeIDs(sortedOrders)
-    # if order_type == 'buy_orders':
-    #     sortedOrders = sortByPriceDesc(sameOrders)
-    # else:
-    #     sortedOrders = sortByPriceAsc(jitaOrders)
-    print('Done')
-    print(len(orders))
-    return orders
+    extractedOrders = []
+    for o in orders:
+        extractedOrders.append(o[order_type])
+    concatedOrders = concat(extractedOrders)
+    print("Filtering...")
+    jitaOrders = list(filter(filter_jita44, concatedOrders))
+    print("Sorting...")
+    sortedOrders = sortByTypeID(jitaOrders)
+    print("Concating same type_id orders...")
+    sameTypeOrders = concatSameTypeIDs(sortedOrders)
+    if order_type == 'buy_order':
+        for o in sameTypeOrders:
+            sortByPriceDesc(o)
+    else:
+        for o in sameTypeOrders:
+            sortByPriceAsc(o)
 
-# todo: extract buy orders from object
+    
+    return sameTypeOrders
+
+
+# def get_items_industry(orders):
+#     type_ids = []
+#     for o in orders:
+#         type_ids.append(o[0]['type_id'])
+#     items = getItems()
+#     for id in type_ids:
+        
+# def find_profits(orders, method, amount):
+    
